af41b4181f40c4d18a15bc16a009599843e00f70
Gabriel <kbeca.gabriel793@gmail.com>
2024-10-14 19:14:19 -0300

    period hours

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours";
 
+const hours = document.querySelector("#hours");
+
 export function hoursLoad({ date }) {
   console.log(date);
 
@@ -15,5 +17,28 @@ export function hoursLoad({ date }) {
     };
   });
 
-  console.log(opening);
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li");
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+
+    if (hour === "9:00") {
+      hourHeaderAdd("Manhã");
+    } else if (hour === "12:00") {
+      hourHeaderAdd("Tarde");
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite");
+    }
+
+    li.textContent = hour;
+    hours.appendChild(li);
+  });
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li");
+  header.classList.add("hour-period");
+  header.textContent = title;
+
+  hours.appendChild(header);
 }
