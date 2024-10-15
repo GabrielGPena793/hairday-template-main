3e87bd50a934d91f86b12258ed5cf9d3de637048
Gabriel <kbeca.gabriel793@gmail.com>
2024-10-15 10:14:16 -0300

    schedules show

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,8 +1,14 @@
+import { scheduleGetByDay } from "../../services/schedule-get-by-day";
 import { hoursLoad } from "../form/hours-load";
+import { scheduleShow } from "./show";
 
 const selectDate = document.querySelector("#date");
-export function scheduleDays() {
+export async function scheduleDays() {
   const date = selectDate.value;
+
+  const dailySchedules = await scheduleGetByDay({ date });
+
+  scheduleShow({ dailySchedules });
 
   hoursLoad({ date });
 }
